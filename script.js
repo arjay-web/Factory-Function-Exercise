703e742a2b9112ad90f8a74503d4727b6fd8e4d1
Arjay Dimaunahan <arjay213112@gmail.com>
2026-02-11 09:45:16 +0800

    Connect the DOM and Logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const addPlayerBtn = document.querySelector('#addPlayer');
 const playerName = document.querySelector('#playerName');
+const playerContainer = document.querySelector('#playerContainer');
+
+
 
 const players = [];
 
@@ -36,14 +39,60 @@ function createPlayer(name){
 function addPlayer(name){
     const newPlayer = createPlayer(name);
     players.push(newPlayer);
+
+    renderPlayer()
+}
+
+function renderPlayer(){
+    playerContainer.innerHTML = "";
+
+    players.forEach(player =>{
+        const playerCard = document.createElement('div');
+        playerCard.classList.add()
+
+        playerCard.innerHTML = `
+        <div>
+            <h3>${player.getName()}</h3>
+            <p>${player.getScore()}</p>
+            <button class="increaseBtn">Increase</button>
+            <button class="decreaseBtn">Decrease</button>
+            <button class="resetBtn">Reset</button>
+            
+        </div>
+        `;
+
+        const increaseBtn = playerCard.querySelector('.increaseBtn');
+        const decreaseBtn = playerCard.querySelector('.decreaseBtn');
+        const resetBtn = playerCard.querySelector('.resetBtn')
+
+        increaseBtn.addEventListener('click',()=>{
+            player.increaseScore();
+            renderPlayer();
+        })
+
+        decreaseBtn.addEventListener('click', ()=> {
+            player.decreaseScore();
+            renderPlayer();
+        })
+
+        resetBtn.addEventListener('click', ()=>{
+            player.reset();
+            renderPlayer()
+        })
+
+        playerContainer.appendChild(playerCard);
+        
+    })
+
+    
 }
 
 addPlayerBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     const player = playerName.value;
-    addPlayer(player);
-    console.log(players)  
+    addPlayer(player);  
 })
+
 
 
 
